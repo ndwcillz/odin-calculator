3ad55942934ec0b030c4e0a13a4c46207dc9bc88
William Zhou <ndxcxnter@gmail.com>
2026-07-05 00:24:34 +0800

    Make Number Buttons Responsive on the Display
    Lets a button press update the numbers on the
    display

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+let first = 0;
+let second = 0;
+let operator = "";
+let currentInput = "";
+
 function add(a, b) {
     return a + b;
 }
@@ -14,14 +19,50 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
+function operate(operator, first, second) {
     if (operator == "+") {
-        return add(a, b);
+        return add(first, second);
     } else if (operator == "-") {
-        return subtract(a, b);
+        return subtract(first, second);
     } else if (operator == "*") {
-        return multiply(a, b);
+        return multiply(first, second);
     } else if (operator == "/") {
-        return divide(a, b);
+        return divide(first, second);
     }
 }
+
+const zeroButton = document.getElementById("zero");
+const sevenButton = document.getElementById("seven");
+const eightButton = document.getElementById("eight");
+const nineButton = document.getElementById("nine");
+const fourButton = document.getElementById("four");
+const fiveButton = document.getElementById("five");
+const sixButton = document.getElementById("six");
+const oneButton = document.getElementById("one");
+const twoButton = document.getElementById("two");
+const threeButton = document.getElementById("three");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
+const addButton = document.getElementById("add");
+const subtractButton = document.getElementById("subtract");
+
+const digitButtons = document.querySelectorAll(".digit");
+
+digitButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        currentInput = currentInput + button.textContent;
+        document.getElementById("display").textContent = currentInput;
+    });
+});
+
+const operatorButtons = document.querySelectorAll(".operator");
+
+operatorButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+
+    });
+});
+
+const decimalButton = document.getElementById("decimal");
+const equalsButton = document.getElementById("equals");
+const clearButton = document.getElementById("clear");
